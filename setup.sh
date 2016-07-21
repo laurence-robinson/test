0395f8617ac43270319cb53608aa49528824f800
laurence-robinson <laurence.robinson@riotgames.com>
2016-07-21 14:40:49 +0100

    Fixing repo add

--- setup.sh
+++ setup.sh
@@ -36,8 +36,7 @@ name=Docker Repository
 baseurl=https://yum.dockerproject.org/repo/main/centos/7/
 enabled=1
 gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg
-EOF" > /etc/yum.repos.d/docker.repo
+gpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
 yum install -y docker-engine
 service docker start
 yum install -y epel-release

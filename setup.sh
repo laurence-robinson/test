9e3c07f94972176701790c014a0bfea3a7e8803d
laurence-robinson <laurence.robinson@riotgames.com>
2016-07-21 15:17:41 +0100

    Fixing pip install

--- setup.sh
+++ setup.sh
@@ -37,8 +37,9 @@ baseurl=https://yum.dockerproject.org/repo/main/centos/7/
 enabled=1
 gpgcheck=1
 gpgkey=https://yum.dockerproject.org/gpg" > /etc/yum.repos.d/docker.repo
-yum install -y python-pip
+rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum install -y epel-release 
+yum install -y python-pip
 yum install -y docker-engine
 service docker start
 /usr/bin/yes | pip install virtualenv
